483b902c6e723684a53c72647a4a873dcad735fb
Bernardo <bernardo_limas@hotmail.com>
2023-05-23 17:42:29 -0300

    first completed module

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
+
+//este doc serve para exportar globalmente através do '.d' do arquivo
